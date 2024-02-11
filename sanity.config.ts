0031afe91daa4a9aaced92e795ed79567b1bb9ba
steppan26 <stephane.baroux@gmail.com>
2024-02-11 22:08:55 +0100

    comment sanity config

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,12 +1,8 @@
-/**
- * This configuration is used to for the Sanity Studio that’s mounted on the `/app/admin/[[...index]]/page.tsx` route
- */
-
 import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
 import {apiVersion, dataset, projectId} from './sanity/env'
-import { singletonTypes, structure } from '@/Utils/sanity/deskStructure'
+// import { singletonTypes, structure } from '@/Utils/sanity/deskStructure'
 import { schemaTypes } from './sanity/schemas'
 
 export const singletonActions = new Set(["publish", "discardChanges", "restore"])
@@ -18,17 +14,17 @@ export default defineConfig({
   schema: { types: schemaTypes },
   title: "D.Velasquez Admin Dashboard",
   plugins: [
-    deskTool({
-      structure
-    }),
+    // deskTool({
+    //   structure
+    // }),
     visionTool({defaultApiVersion: apiVersion}),
   ],
   document: {
     // For singleton types, filter out actions that are not explicitly included
     // in the `singletonActions` list defined above
-    actions: (input, context) =>
-      singletonTypes.has(context.schemaType)
-        ? input.filter(({ action }) => action && singletonActions.has(action))
-        : input,
+    // actions: (input, context) =>
+    //   singletonTypes.has(context.schemaType)
+    //     ? input.filter(({ action }) => action && singletonActions.has(action))
+    //     : input,
   },
 })
