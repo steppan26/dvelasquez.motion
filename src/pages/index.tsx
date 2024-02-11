608d52f89a347e14ae749c57a722cf0289da6d6f
steppan26 <stephane.baroux@gmail.com>
2024-02-11 23:31:55 +0100

    remove sanity

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,10 @@
 import { Landing } from "../Containers"
-import { client } from "../utils/sanity/client"
-import { getNavData } from "../utils/sanity/commands"
-import { PageTitle } from '../utils/sanity/types'
 
-interface Props {
-  pageTitle: PageTitle[]
-}
-
-export default function Home(props: Props) {
-  const pageHeader = props.pageTitle[0]
+export default function Home() {
 
   return (
     <>
     <Landing />
     </>
   )
-}
-
-export const getStaticProps = async () => {
-  const [navbarData, pageTitle] = await Promise.all([
-    getNavData(),
-    client.fetch<PageTitle>(`*[_type == "pageTitle"]`)
-  ])
-  return({
-    props: {
-      navbarData,
-      pageTitle
-    },
-    revalidate: 5
-  })
 }
