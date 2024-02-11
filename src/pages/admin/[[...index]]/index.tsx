dd376f923abd902e3c92ef6f315e51462d8a4142
steppan26 <stephane.baroux@gmail.com>
2024-02-11 23:25:42 +0100

    comment studio admin

--- src/pages/admin/[[...index]]/index.tsx
+++ src/pages/admin/[[...index]]/index.tsx
@@ -13,13 +13,17 @@ import { NextStudio } from 'next-sanity/studio'
 import config from '../../../../sanity.config'
 import Head from 'next/head'
 
-export default function StudioPage() {
-  return (
-    <>
-      <Head>
-        <title>Admin.Velasquez</title>
-      </Head>
-      <NextStudio config={config} />
-    </>
-  )
+export default function index() {
+    return <></>
 }
+
+// export default function StudioPage() {
+//   return (
+//     <>
+//       <Head>
+//         <title>Admin.Velasquez</title>
+//       </Head>
+//       {/*<NextStudio config={config} />*/}
+//     </>
+//   )
+// }
