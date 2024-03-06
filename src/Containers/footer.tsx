f329b580e1c7bd47c9ce71e84de01861b178ec3f
stephane <stephane@yavin.com>
2024-03-06 11:42:00 +0100

    reset footer

--- src/Containers/footer.tsx
+++ src/Containers/footer.tsx
@@ -15,9 +15,9 @@ export const Footer:React.FC = () => {
       />
       <TextWrapper>
         <InternalLink href="/">Home</InternalLink>
-        <InternalLink href="/">Works</InternalLink>
-        <InternalLink href="/">About</InternalLink>
-        <InternalLink href="/">Contact</InternalLink>
+        <InternalLink href="/works">Works</InternalLink>
+        <InternalLink href="/about">About</InternalLink>
+        <InternalLink href="/about#contact">Contact</InternalLink>
       </TextWrapper>
       <TextWrapper>
         <ExternalLink>Instagram</ExternalLink>
