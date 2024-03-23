16024a353765991a95ee5d3bbaf2be4d7fd1f6b7
stephane <stephane@yavin.com>
2024-03-23 21:33:19 +0100

    fix bug

--- src/utils/hooks/useScrollToTop.tsx
+++ src/utils/hooks/useScrollToTop.tsx
@@ -1,9 +1,7 @@
-import { useRouter } from "next/router";
 import { useCallback, useEffect, useState } from "react";
 
 export const useScrollToTop = () => {
   const [scrollableElements, setScrollableElements] = useState<Element[]>([])
-  const router = useRouter()
 
   const scrollToTop = useCallback((behavior: ScrollBehavior='smooth') => {
     scrollableElements.forEach(el => el.scrollTo({ top: 0, behavior }))
@@ -14,10 +12,7 @@ export const useScrollToTop = () => {
 
     const elements = getScrollableElements(window.document.documentElement)
     setScrollableElements(elements)
-    router.events.on('routeChangeComplete', scrollToTop)
-
-    return () => router.events.off('routeChangeComplete', scrollToTop)
-  }, [router, scrollToTop])
+  }, [])
 
   function getScrollableElements(container: HTMLElement) {
     const elements = container ? container.querySelectorAll('*') : document.querySelectorAll('*')
