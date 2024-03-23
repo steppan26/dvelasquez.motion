30a26957099d211ebec555c8eec0f4d7dcfc18b6
stephane <stephane@yavin.com>
2024-03-23 15:18:57 +0100

    move viewport meta tag to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,6 +16,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     <>
     <Head>
         <link rel='icon' href={favicon.src} />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
       </Head>
     <Stores>
       <Component {...pageProps} />
