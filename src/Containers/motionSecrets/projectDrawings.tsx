4feb4ae9b45cba916c10f11661751ee932ff14e1
stephane <stephane@yavin.com>
2024-05-01 22:48:18 +0200

    fix drawings alignmnet

--- src/Containers/motionSecrets/projectDrawings.tsx
+++ src/Containers/motionSecrets/projectDrawings.tsx
@@ -6,15 +6,18 @@ import Drawing2 from '/public/projects/motion_secrets/drawing_2.webp'
 import Drawing3 from '/public/projects/motion_secrets/drawing_3.webp'
 import Drawing4 from '/public/projects/motion_secrets/drawing_4.webp'
 import { Sizes } from "../../Assets"
+import { isSafari } from "../../utils/helpers"
 
 export const ProjectDrawings:React.FC = () => {
+
   return (
-    <Container>
+    <Container data-issafari={isSafari()}>
       <Image
         src={Drawing1}
         alt="sketches of ideas during design stages"
         id="drawingOne"
         data-lazy="motion-secrets_drawing"
+        data-issafari={isSafari()}
         style={{
           maxWidth: "100%",
           height: "auto"
@@ -60,18 +63,29 @@ const Container = styled.div`
     grid-template-areas: 'drawing1 . drawing2'
     '. text .'
     'drawing3 . drawing4';
-  margin-bottom: 15dvh;
 
   #drawingOne {
     grid-area: drawing1;
     align-self: start;
     justify-self: center;
-    transform:  translate(15%, 32%) rotate(25deg) scale(0.9);
+    -ms-transform: translateX(15%) translateY(32%) rotate(25deg) scale(0.9);
+    -webkit-transform: translateX(15%) translateY(32%) rotate(25deg) scale(0.9);
+    -moz-transform: translateX(15%) translateY(32%) rotate(25deg) scale(0.9);
+    -o-transform: translateX(15%) translateY(32%) rotate(25deg) scale(0.9);
+    transform: translateX(15%) translateY(32%) rotate(25deg) scale(0.9);
+    transform: translateX(0) translateY(-4rem) rotate(25deg) scale(0.9);
+
+    /* &[data-issafari='true'] {
+    } */
   }
   #drawingTwo {
     grid-area: drawing2;
     align-self: end;
     justify-self: center;
+    -ms-transform: rotate(28deg) scale(1.2);
+    -webkit-transform: rotate(28deg) scale(1.2);
+    -moz-transform: rotate(28deg) scale(1.2);
+    -o-transform: rotate(28deg) scale(1.2);
     transform: rotate(28deg) scale(1.2);
   }
   #drawingThree {
@@ -79,12 +93,20 @@ const Container = styled.div`
     align-self: start;
     justify-self: start;
     padding-top: 2rem;
+    -ms-transform: translateX(50%) rotate(-3.5deg) scale(1.2);
+    -webkit-transform: translateX(50%) rotate(-3.5deg) scale(1.2);
+    -moz-transform: translateX(50%) rotate(-3.5deg) scale(1.2);
+    -o-transform: translateX(50%) rotate(-3.5deg) scale(1.2);
     transform: translateX(50%) rotate(-3.5deg) scale(1.2);
   }
   #drawingFour {
     grid-area: drawing4;
     align-self: start;
     justify-self: start;
+    -ms-transform: translateX(-35%) rotate(-16deg) scale(1.4);
+    -webkit-transform: translateX(-35%) rotate(-16deg) scale(1.4);
+    -moz-transform: translateX(-35%) rotate(-16deg) scale(1.4);
+    -o-transform: translateX(-35%) rotate(-16deg) scale(1.4);
     transform: translateX(-35%) rotate(-16deg) scale(1.4);
   }
 
